5ae5e3932849ac33e9178d8c65a2a4931673cec8
Kevin Wei <KevinWeiHadExtra>
2021-02-17 19:18:16 -0800

    Initial Upload

---
+++ Testing/Assets/BallController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    [SerializeField] private GameObject ballPrefab;
+    private GameObject _ball;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (_ball==null && Input.GetMouseButtonDown(0))
+        {
+            _ball = Instantiate(ballPrefab) as GameObject;
+        }
+        if (_ball != null && Input.GetMouseButtonDown(1))
+        {
+            Destroy(_ball);
+        }
+    }
+}
